c8a8be58277b8c55ecacbcac2a3013ad162d7634
Abhishek <abhiwable04@gmail.com>
2024-02-13 16:20:11 +0530

    this 3rd commit

--- src/test/java/Api/Test/DDUserTest.java
+++ src/test/java/Api/Test/DDUserTest.java
@@ -8,14 +8,13 @@ import io.restassured.response.Response;
 import junit.framework.Assert;
 
 public class DDUserTest {
-	
-	
-	@Test(priority=1, dataProvider = "Data", dataProviderClass= DataProviders.class)
-	public void PostUserTest(String UserId, String UserName, String fname, String lname, String useremail,String pwd, String ph) {
-		
-		
+
+	@Test(priority = 1, dataProvider = "Data", dataProviderClass = DataProviders.class)
+	public void PostUserTest(String UserId, String UserName, String fname, String lname, String useremail, String pwd,
+			String ph) {
+
 		User UserPayload = new User();
-		
+
 		UserPayload.setId(Integer.parseInt(UserId));
 		UserPayload.setUsername(UserName);
 		UserPayload.setFirstName(fname);
@@ -23,24 +22,32 @@ public class DDUserTest {
 		UserPayload.setEmail(useremail);
 		UserPayload.setPassword(pwd);
 		UserPayload.setPhone(ph);
-		
+
 		Response Res = UserEndpoints.createUser(UserPayload);
-		
-//		Res.then().log().all();
-		
+
+		// Res.then().log().all();
+
 		Assert.assertEquals(Res.getStatusCode(), 200);
-		
+
 	}
-	
-	
-	@Test(priority=2, dataProvider="UserNames" ,dataProviderClass = DataProviders.class )
+
+	@Test(priority = 2, dataProvider = "UserNames", dataProviderClass = DataProviders.class)
+	public void GetUserTest(String UserName) {
+
+		Response res = UserEndpoints.getUser(UserName);
+
+		Assert.assertEquals(res.statusCode(), 200);
+
+		System.out.println(res);
+	}
+
+	@Test(priority = 3, dataProvider = "UserNames", dataProviderClass = DataProviders.class)
 	public void DeleteUser(String UserName) {
-		
+
 		Response res = UserEndpoints.DeleteUser(UserName);
-		
+
 		Assert.assertEquals(res.getStatusCode(), 200);
-		
-		
+
 	}
 
 }
